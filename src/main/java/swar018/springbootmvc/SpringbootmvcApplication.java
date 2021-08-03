0e67a3659a96931b817988b6c3d9d992e80cc670
jinwoo park <swat018@icloud.com>
2021-08-03 20:49:58 +0900

    Initial commit

---
+++ src/main/java/swar018/springbootmvc/SpringbootmvcApplication.java
@@ -0,0 +1,12 @@
+package swar018.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcApplication.class, args);
+	}
+}
